9e717100b7a9bcd0bd275053239a6536ba872a9f
abdullah0307 <abdullahjavaid0307@gmail.com>
2024-07-18 18:55:06 +0500

    my first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+import base64
+import os
+
+import openai
+import streamlit as st
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+
+def decode_api_key(encoded_api_key):
+    decoded_bytes = base64.b64decode(encoded_api_key.encode('utf-8'))
+    decoded_str = str(decoded_bytes, 'utf-8')
+    return decoded_str
+
+
+openai_api_key = decode_api_key(os.environ["OPEN_AI_API_KEY"])
+
+st.title("💬 My Chatbot")
+st.caption("🚀 A Streamlit chatbot powered by Me")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    client = OpenAI(api_key=openai_api_key)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
